53159bf3df7a04e8a7b1bfb043233c55d8e9b487
drprasad <tech.dr.prasad@live.com>
2016-07-12 13:10:55 -0400

    Listing movies and styling index page

---
+++ app/models/movie.rb
@@ -0,0 +1,10 @@
+class Movie < ActiveRecord::Base
+	validates :title, :release_year, :price, :description, :imdb_id, :poster_url, presence: true
+	def poster
+    "http://ia.media-imdb.com/images/M/#{poster_url}"
+  end
+
+  def imdb
+    "http://www.imdb.com/title/#{imdb_id}/"
+  end
+end
