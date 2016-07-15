342690915de903c419daf92adaf99b780ee98e8a
drprasad <tech.dr.prasad@live.com>
2016-07-15 10:34:55 -0400

    Added show page

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,4 +3,10 @@ class MoviesController < ApplicationController
 		@movies = Movie.all
 		#render json: @movies
 	end
+
+	def show
+		#render plain: params[:id]
+		@movie = Movie.find(params[:id])
+		#render json: @movie
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'welcome#index'
   get 'movies', to: 'movies#index'
+  get 'movies/:id', to: 'movies#show', as: :movie
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
