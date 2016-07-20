58eed1045e8bf083768485b9c21ae3a82e692316
drprasad <tech.dr.prasad@live.com>
2016-07-20 01:28:11 -0400

    Added login and signup using devise

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,7 @@
 class Movie < ActiveRecord::Base
 	validates :title, :release_year, :price, :description, :imdb_id, :poster_url, presence: true
+	validates :imdb_id, :poster_url, uniqueness: true
+
 	def poster
     "http://ia.media-imdb.com/images/M/#{poster_url}"
   end
